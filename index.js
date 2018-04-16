e40d3de0c3441f02b9eee24e8d076fbe048115c5
justin meskan <justinmeskan@gmail.com>
2018-04-16 16:38:51 -0700

    i creaated the closing animation then wrote the code so that only one door can be open at a time and if  one is open the others close.

--- index.js
+++ index.js
@@ -1,5 +1,65 @@
 (function(){
 	$('.door').on('click', function(e){
-		$(this).addClass('animator')
+		var door1 = $('#door1').hasClass('open');
+		var door2 = $('#door2').hasClass('open');
+		var door3 = $('#door3').hasClass('open');
+		var id = this.id;
+		var closed = $(this).hasClass('c');
+		var open = $(this).hasClass('o');
+		if(closed){
+			$(this).removeClass('c close');
+			$(this).addClass('o');
+			$(this).addClass('open');
+		}else if(open){
+			$(this).removeClass('o open');
+			$(this).addClass('c');
+			$(this).addClass('close');
+		}
+		if(id == 'door1'){
+			if(id === 'door1' && closed && door2){
+				$('#door2').removeClass('o open');
+				$('#door2').addClass('c close');
+				$(this).removeClass('c close')
+				$(this).addClass('o open')
+			}
+			if(id === 'door1' && closed && door3){
+				$('#door3').removeClass('o open');
+				$('#door3').addClass('c close');
+				$(this).removeClass('c close')
+				$(this).addClass('o open')
+			}
+		}
+
+		if(id == 'door2'){
+			if(id === 'door2' && closed && door1){
+				$('#door1').removeClass('o open');
+				$('#door1').addClass('c close');
+				$(this).removeClass('c close')
+				$(this).addClass('o open')
+			}
+			if(id === 'door2' && closed && door3){
+				$('#door3').removeClass('o open');
+				$('#door3').addClass('c close');
+				$(this).removeClass('c close')
+				$(this).addClass('o open')
+			}
+		}
+
+		if(id == 'door3'){
+			if(id === 'door3' && closed && door1){
+				$('#door1').removeClass('o open');
+				$('#door1').addClass('c close');
+				$(this).removeClass('c close')
+				$(this).addClass('o open')
+			}
+			if(id === 'door3' && closed && door2){
+				$('#door2').removeClass('o open');
+				$('#door2').addClass('c close');
+				$(this).removeClass('c close')
+				$(this).addClass('o open')
+			}
+		}
+		console.log('this',this.id)
+		console.log('e',e)
 	})
 })();
