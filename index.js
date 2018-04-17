fc92db26217d651bc5bbfdbf2fb5652a1e242f9f
justin meskan <justinmeskan@gmail.com>
2018-04-17 10:59:46 -0700

    i got the winning background to pop-up randomly.

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 (function(){
+	//random Bg generator
+	function randGen(){
+		var index = Math.floor(Math.random() * arguments.length);
+		return arguments[index];
+	}
+	var goodDoor = randGen('#doorbg1', '#doorbg2', '#doorbg3');
+	$(goodDoor).css({'background-image':'url(moneygirl.jpeg)'});
+	console.log('secert door', goodDoor);
+	//default door clicks
 	$('.door').on('click', function(e){
 		var door1 = $('#door1').hasClass('open');
 		var door2 = $('#door2').hasClass('open');
@@ -10,56 +19,48 @@
 			$(this).removeClass('c close');
 			$(this).addClass('o');
 			$(this).addClass('open');
-		}else if(open){
-			$(this).removeClass('o open');
-			$(this).addClass('c');
-			$(this).addClass('close');
 		}
-		if(id == 'door1'){
-			if(id === 'door1' && closed && door2){
+		if(id === 'door1'){
+			if(closed && door2){
 				$('#door2').removeClass('o open');
 				$('#door2').addClass('c close');
 				$(this).removeClass('c close')
 				$(this).addClass('o open')
 			}
-			if(id === 'door1' && closed && door3){
+			if(closed && door3){
 				$('#door3').removeClass('o open');
 				$('#door3').addClass('c close');
 				$(this).removeClass('c close')
 				$(this).addClass('o open')
 			}
 		}
-
-		if(id == 'door2'){
-			if(id === 'door2' && closed && door1){
+		if(id === 'door2'){
+			if(closed && door1){
 				$('#door1').removeClass('o open');
 				$('#door1').addClass('c close');
 				$(this).removeClass('c close')
 				$(this).addClass('o open')
 			}
-			if(id === 'door2' && closed && door3){
+			if(closed && door3){
 				$('#door3').removeClass('o open');
 				$('#door3').addClass('c close');
 				$(this).removeClass('c close')
 				$(this).addClass('o open')
 			}
 		}
-
-		if(id == 'door3'){
-			if(id === 'door3' && closed && door1){
+		if(id === 'door3'){
+			if(closed && door1){
 				$('#door1').removeClass('o open');
 				$('#door1').addClass('c close');
 				$(this).removeClass('c close')
 				$(this).addClass('o open')
 			}
-			if(id === 'door3' && closed && door2){
+			if(closed && door2){
 				$('#door2').removeClass('o open');
 				$('#door2').addClass('c close');
 				$(this).removeClass('c close')
 				$(this).addClass('o open')
 			}
 		}
-		console.log('this',this.id)
-		console.log('e',e)
 	})
 })();
