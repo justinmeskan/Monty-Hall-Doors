2b3db597c274ea7f57bd139409f00060944c7bf7
justin meskan <justinmeskan@gmail.com>
2018-04-19 21:26:28 -0700

    its a need little redo of the monty three door thing

--- index.js
+++ index.js
@@ -3,18 +3,25 @@
 		var index = Math.floor(Math.random() * arguments.length);
 		return arguments[index];
 	}
+	window.addEventListener('load', function(event){
+		$('body').css({'height':window.innerHeight+'px'});
+	});
+	window.addEventListener('load resize', function(event){
+		$('body').css({'height':window.innerHeight+'px'});
+	});
 	var goodDoor = randGen('#doorbg1', '#doorbg2', '#doorbg3');
 	$(goodDoor).css({'background-image':'url(moneygirl.jpeg)'});	
 	var turn = 1;
 	var doors = ['1', '2', '3'];
 	var ran;
+	var winningDoor;
 	var doorClick = $('.door').on('click', function(e){
 		if(turn === 1){
 			$('#message').text(`So you think that"s the right door huh? I"ll tell you
 								what...One of the wrong doors will open.... You can 
 								change your pick if like..MooooAAaaaahahahahah`);
 			var clickedDoor = this.id.slice(this.id.length - 1);;
-			var winningDoor = goodDoor.slice(goodDoor.length - 1);;
+			winningDoor = goodDoor.slice(goodDoor.length - 1);;
 			var idx = doors.indexOf(clickedDoor);
 			doors.splice(idx, 1);
 			if(doors.indexOf(winningDoor) !== -1){
@@ -42,11 +49,17 @@
 			}else{
 				openDoor = ran;
 			}
-			console.log('openDoor',openDoor);
+			if(this.id.slice(this.id.length - 1) === String(winningDoor)){
+				$('#message').text('YOU WIN!!!!!! You picked the right door...Go get your prize!!!')
+				$('#'+this.id).addClass('open');
+			}else{
+				$('#message').text('OOOOHHH NOOOOOOO!!!! NOT THAT DOOR!!!!! REAP THE CONSEQUENCES!!!! MooooAAaaaahahahahah!!!!')
+				$('#'+this.id).addClass('open');
+			}
+			$('.door').off('click');
 		}
 	});
 })();
-
 
 
 
